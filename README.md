5b520878b3e0cacb321098673855c761330dbbd4
KOZ39 <koz39c@gmail.com>
2018-08-24 10:53:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Unity-Assets-Split
+
+Inspired by [PoroCYon](https://gitlab.com/users/PoroCYon/snippets)
+
+## License
+[MIT](https://github.com/KOZ39/Unity-Assets-Split/blob/master/LICENSE)
---
+++ src/split.py
@@ -0,0 +1,25 @@
+import glob
+import os
+from math import floor
+
+MB = 1024 * 1024
+
+for i in glob.iglob(f'assets/**/sharedassets*.assets', recursive=True):
+    fileSize = os.path.getsize(i)
+
+    if fileSize > MB:
+        q = floor(fileSize / MB)
+        r = floor(fileSize % MB)
+        n = 0
+
+        with open(i, 'rb') as f:
+            d = f.read()
+
+        while(n < q):
+            with open(f'{i}.split{n}', 'wb') as f:
+                f.write(d[MB * n:MB * (n + 1)])
+            n += 1
+
+        if r > 0:
+            with open(f'{i}.split{n}', 'wb') as f:
+                f.write(d[MB * n:])
